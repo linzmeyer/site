192bcabc728bffa19b02f5ed4c99aa0c00a2dc08
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-26 12:04:52 -0500

    removed space and text

--- src/Views/Availability/Availability.js
+++ src/Views/Availability/Availability.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
 import calendar from '../../img/calendar.png';
-import './Availability.css';
 
 class Availability extends Component {
 
@@ -12,25 +11,17 @@ class Availability extends Component {
         <Nav />
         <div>
           <div className="content-container" >
-            {/* Top Container */}
-              <div className="info">
-                <h1>Current Availability</h1>
-                <ul>
-                  <li>Looking for opportunities!</li>
-                  <li>Will be available to start work Monday, July 1 2019.</li>
-                  <li><Link to="/contact" >Contact me</Link> to help on your upcoming projects.</li>
-                </ul>
-                <img src={ calendar } alt="calendar" className="calendar" ></img>
-              </div>
-            
-            <section className="bottom-container" >
-
-            </section>
-
-            {/* Portfolio (github API) */}
+            <div className="info">
+              <h1>Current Availability</h1>
+              <ul>
+                <li>Looking for opportunities!</li>
+                <li>Will be available to start work Monday, July 1 2019.</li>
+                <li><Link to="/contact" >Contact me</Link> to help on your upcoming projects.</li>
+              </ul>
+              <img src={ calendar } alt="calendar" className="calendar" ></img>
             </div>
-
           </div>
+        </div>
       </div>
     );
   }
