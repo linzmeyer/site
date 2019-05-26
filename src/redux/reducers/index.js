662b3e63c370a01cb2b0bc6edb2778e32eeee0c5
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-25 21:52:21 -0500

    added experience reducer to list

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,11 @@ import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
 import testimonial from './TestimonialsReducer';
+import experience from './experienceReducer';
 
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
+  experience, 
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   testimonial,
   user, // will have an id and username if someone is logged in
