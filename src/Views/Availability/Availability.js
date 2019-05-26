8385b2dec241f8588d5b24e8624d389605c2586d
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-26 18:08:47 -0500

    changed h1 to h2

--- src/Views/Availability/Availability.js
+++ src/Views/Availability/Availability.js
@@ -12,12 +12,14 @@ class Availability extends Component {
         <div>
           <div className="content-container" >
             <div className="info">
-              <h1>Current Availability</h1>
+              <h2>Current Availability</h2>
               <ul>
                 <li>Looking for opportunities!</li>
                 <li>Will be available to start work Monday, July 1 2019.</li>
                 <li><Link to="/contact" >Contact me</Link> to help on your upcoming projects.</li>
               </ul>
+            </div>
+            <div className="info" >
               <img src={ calendar } alt="calendar" className="calendar" ></img>
             </div>
           </div>
