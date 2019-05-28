97c562d1732c6dc26b8a6e00a885042e63985290
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-27 23:56:41 -0500

    fixed type

--- src/Views/About/sections/Experience/sections/WireTech.js
+++ src/Views/About/sections/Experience/sections/WireTech.js
@@ -13,7 +13,7 @@ class WireTech extends Component {
         I took on many other responsibilities as well. I was nominated several times to serve on a council that represented the technicians of our shop. I would collaborate with the company owners to come up with ideas for higher productivity and to relay the concerns of shop technicians directly to the owners. This was a new role at our company, and I enjoyed it. 
       </p>
       <p>
-        I was also a trainer for new-hire wire technicians. Rarely we would onboard an experienced technician, so we needed someone to teach new-hires the basics of constructing and wiring with quality and care. While in this role, I wrote up a step-by-step document that describes the process on how to build control panels and troubleshooting for certain problems during contstruction.
+        I was also a trainer for new-hire wire technicians. Rarely we would onboard an experienced technician, so we needed someone to teach new-hires the basics of constructing and wiring with quality and care. While in this role, I wrote up a step-by-step document that describes the process on how to build control panels and troubleshooting for certain problems during construction.
       </p>
       <p>9/2012 - 2/2018</p>
     </section>
