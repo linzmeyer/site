eacb65982dcf1a193bc17939988dd8dead12d0c4
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-06-10 10:13:31 -0500

    updated where i want to work

--- src/Views/About/sections/WhatImLookingFor/WhatImLookingFor.js
+++ src/Views/About/sections/WhatImLookingFor/WhatImLookingFor.js
@@ -7,9 +7,9 @@ class WhatImLookingFor extends Component {
       <section className="info" >
         <h2>What I'm Looking For</h2>
         <ul>
-          <li>Software development on the East side of the Twin Cities. (St. Paul, Woodbury, Maplewood, Oakdale, etc.)</li>
+          <li>Software development role in the Twin Cities.</li>
           <li>Okay with working remotely.</li>
-          <li>Work that challenges me to learn new technologies and development practices.</li>
+          <li>Work that challenges me to learn new technologies and good software development practices.</li>
           <li>A place with a good onboarding process.</li>
           <li>Friendly coworkers</li>
         </ul>
